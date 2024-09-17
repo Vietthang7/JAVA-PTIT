0f14ce01e3851f2d91932c3f53f67bb35126e00b
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-09-17 10:23:33 +0700

    update

--- J02017.java
+++ J02017.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.*;
 
-/**
- *
- * @author Admin
- */
 public class J02017 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
