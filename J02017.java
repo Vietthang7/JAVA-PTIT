4da6e16129f9988639bfe38e1e9a3a393b37591c
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-09-17 13:30:37 +0700

    .

--- J02017.java
+++ J02017.java
@@ -1,7 +1,20 @@
+
 import java.util.*;
 
 public class J02017 {
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Stack<Integer> st = new Stack<>();
+        while (n-- > 0) {
+            int x = sc.nextInt();
+            if (!st.isEmpty() && (x + st.peek()) % 2 == 0) {
+                st.pop();
+            } else {
+                st.push(x);
+            }
+        }
+        System.out.println(st.size());
     }
 }
