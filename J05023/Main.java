1e114d762f88e7a3c4e2c52d829707e2b999c1f0
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-12-02 23:42:40 +0700

    .

--- J05023/Main.java
+++ J05023/Main.java
@@ -1,5 +1,26 @@
 package J05023;
 
+import java.util.*;
+
 public class Main {
-  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.nextLine();
+    ArrayList<SinhVien> arr = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      arr.add(new SinhVien(sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextLine()));
+    }
+    int k = sc.nextInt();
+    sc.nextLine();
+    while (k-- > 0) {
+      String khoa = sc.nextLine();
+      System.out.println("DANH SACH SINH VIEN KHOA " + khoa + ":");
+      for (SinhVien x : arr) {
+        if (x.getLop().substring(1, 3).contentEquals(khoa.substring(2))) {
+          System.out.println(x);
+        }
+      }
+    }
+  }
 }
---
+++ J05024/SinhVien.java
@@ -0,0 +1,5 @@
+package J05024;
+
+public class SinhVien {
+  
+}
